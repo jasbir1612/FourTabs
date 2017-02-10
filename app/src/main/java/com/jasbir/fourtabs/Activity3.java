22125fd1620205a9f8be1ad591b1aec49beb8181
jasbir1612 <jasbir1612@gmail.com>
2017-02-11 02:38:47 +0530

    view pager added

---
+++ app/src/main/java/com/jasbir/fourtabs/Activity3.java
@@ -0,0 +1,18 @@
+package com.jasbir.fourtabs;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Activity3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_3, container, false);
+    }
+}
